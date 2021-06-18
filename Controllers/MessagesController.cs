d74506aac3f7b4b75b26fecc4ee7df807585fda3
tales.silva <tales.silva@UDNDELLJMTH813>
2021-06-18 13:02:23 -0300

    Initial commit RabbitMQ Studies

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ_Mensageria.Models;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_Mensageria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private const string QUEUE_NAME = "messages";
+        private readonly ConnectionFactory _factory;
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] SendMessageModel sendMessageModel)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Declara a fila para que caso ela não exista ainda, eu crie ela
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    //Formatar os dados para envio para a fila
+                    var stringMessage = JsonSerializer.Serialize(sendMessageModel);
+                    var byteArray = Encoding.UTF8.GetBytes(stringMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: byteArray
+                        );
+                }
+            }
+            return Accepted();
+        }
+    }
+}
---
+++ Models/SendMessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ_Mensageria.Models
+{
+    public class SendMessageModel
+    {
+        public string FromUser { get; set; }
+        public string ToUser { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
